fe4e5cdaf96aad73d6216135f536ec7c4f2733f4
sanjitsara03 <sanjitsara03@gmail.com>
2025-03-29 23:58:22 -0400

    Implemented openai chatgpt 4o mini

--- app.py
+++ app.py
@@ -2,9 +2,35 @@ import gradio as gr
 from transformers import pipeline
 import numpy as np
 from openai import OpenAI
-import elevenlabs
+import elevenlabs 
 import tempfile
 import apikeys 
 
 client = OpenAI(api_key=apikeys.openaiAPI)
-elevenlabs.set_api_key(apikeys.elevenlabsAPI)
+elevenlabs.api_key = apikeys.elevenlabsAPI
+
+
+#Initializing ASR model
+whisper = pipeline("automatic-speech-recognition", "openai/whisper-large-v3")
+
+#Handling transcription
+def transcribe_respond(audio):
+    sr, y = audio
+    y = y.astype(np.float32)
+    y /= np.max(np.abs(y)) #Normalizing
+
+    transcription = whisper({"sampling_rate": sr, "raw": y})["text"]
+    print("transcribed text:", transcription)
+
+    #Sending transcript to GPT model and getting a reponse
+    response = client.chat.completion.create(
+        model = "gpt-4o-mini",
+        messages=[
+            {
+                "role": "system", "content": "You are a highly intelligent AI system, that keeps its answer short and max 1000 characters"},
+                { "role": "user", "content": transcription}
+        ]
+        
+    )
+    txtResponse =response.choices[0].message.content
+    print(txtResponse)
