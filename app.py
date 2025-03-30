0b5a3a960fa77cc406cfede42511b6227f095ba1
sanjitsara03 <sanjitsara03@gmail.com>
2025-03-30 02:58:15 -0400

    Working app

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import gradio as gr
 from transformers import pipeline
 import numpy as np
 from openai import OpenAI
-import elevenlabs 
+from elevenlabs.client import ElevenLabs
 import tempfile
 import apikeys 
 
 client = OpenAI(api_key=apikeys.openaiAPI)
-elevenlabs.api_key = apikeys.elevenlabsAPI
+tts_client = ElevenLabs(api_key=apikeys.elevenlabsAPI)
 
 
 #Initializing ASR model
@@ -23,11 +23,11 @@ def transcribe_respond(audio):
     print("transcribed text:", transcription)
 
     #Sending transcript to GPT model and getting a reponse
-    response = client.chat.completion.create(
-        model = "gpt-4o-mini",
+    response = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
         messages=[
             {
-                "role": "system", "content": "You are a highly intelligent AI system, that keeps its answer short and max 1000 characters"},
+                "role": "system", "content": "You are a highly intelligent AI system called Sarah that keeps its answer short and under 1000 characters."},
                 { "role": "user", "content": transcription}
         ]
         
@@ -36,17 +36,23 @@ def transcribe_respond(audio):
     print(txtResponse)
 
     #Converting response into audio
-    audioResponse = elevenlabs.generate(text=txtResponse, voice = "Rachel")
+    audioResponse = b"".join(tts_client.generate(
+    text=txtResponse,
+    voice="Sarah",
+    model="eleven_multilingual_v2"
+    ))
+    
     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio:
         temp_audio.write(audioResponse)
         tempAudioPath = temp_audio.name
+
     return tempAudioPath
 
 inter = gr.Interface(
     fn = transcribe_respond,
     inputs=gr.Audio(sources=["microphone"]),
-    outputs=gr.Audio(sources=["upload"])
+    outputs=gr.Audio(type="filepath", autoplay=True)
 )
 
-if __name__ == "__main":
+if __name__ == "__main__":
     inter.launch()
